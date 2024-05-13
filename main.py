ea3f81f99b1a034ad8ff5c835ece2cb90209b440
Abdeladim-Fadlane <abdofadlane128@gmail.com>
2024-05-13 09:39:35 +0100

    2024-05-13 09:39

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ url_template = "https://wuzzuf.net/search/jobs/?q=python&a=hpb&page={}"
 
 jobs = []
 
-for page in range(1, 2): 
+for page in range(0, 2): 
     url = url_template.format(page)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
@@ -15,12 +15,12 @@ for page in range(1, 2):
         title = div.find('a', {'class': 'css-o171kl'}).text.strip()
         city = div.find('span', {'class': 'css-5wys0k'}).text.strip()
         requirements = [a.text.strip() for a in div.find_all('a', {'class': 'css-o171kl'})]
-        place = div.find('span', {'class': 'css-o1vzmt eoyjyou0'})
+        place = div.find('span', {'class': 'css-o1vzmt eoyjyou0'}).text.strip()
 
-        jobs.append({"title": title, "city": city, "requirements": requirements,})
+        jobs.append({"title": title, "city": city, "requirements": requirements, "place": place})
 
 with open('jobs.csv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=['title', 'city', 'requirements'])
+    writer = csv.DictWriter(file, fieldnames=['title', 'place','city', 'requirements'])
     writer.writeheader()
     for job in jobs:
         writer.writerow(job)
