385837f0b17a556e79a67f4e63e367f6cc883175
Abdeladim-Fadlane <abdofadlane128@gmail.com>
2024-05-13 09:32:11 +0100

    2024-05-13 09:32

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ url_template = "https://wuzzuf.net/search/jobs/?q=python&a=hpb&page={}"
 
 jobs = []
 
-for page in range(1, 6): 
+for page in range(1, 10): 
     url = url_template.format(page)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     divs = soup.find_all('div', {'class': 'css-1gatmva e1v1l3u10'})
     for div in divs:
         title = div.find('a', {'class': 'css-o171kl'}).text.strip()
-        # link = div.find('a', {'class': 'css-o171kl'})['href']
         city = div.find('span', {'class': 'css-5wys0k'}).text.strip()
         requirements = [a.text.strip() for a in div.find_all('a', {'class': 'css-o171kl'})]
+        
+
         jobs.append({"title": title, "city": city, "requirements": requirements})
 
 with open('jobs.csv', 'w', newline='') as file:
